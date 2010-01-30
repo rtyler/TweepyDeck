ba3fd49598f2853b3571f469d6a1de81a2a1c2a6
R. Tyler Ballance <tyler@monkeypox.org>
2010-01-30 14:58:21 -0800

    Refactor the status code out into its own class
    Added some code to draw the status in a rounded rectangle, borrowed
    from here:
        http://faq.pygtk.org/index.py?req=revision&file=faq10.024.htp&rev=1.1

--- TweepyDeck/views.py
+++ TweepyDeck/views.py
@@ -3,8 +3,10 @@
 # Standard library imports
 import gettext
 import logging
+import math
 
 # PyGTK imports
+import cairo
 import pygtk
 pygtk.require('2.0')
 import gobject
@@ -42,16 +44,6 @@ class AbstractRow(bases.BaseChildWidget):
         what = text['text']
         return cls(**locals())
 
-    def clickedLink(self, label, uri, data, **kwargs):
-        if not uri.startswith('tweepy://'):
-            return False
-
-        command = uri[9:]
-        command, arg = command.split('/')
-        if command == 'search':
-            util.get_global('app')._spawnSearch('#%s' % arg)
-        return True
-
     def _renderContainer(self):
         container = gtk.HBox()
         container.set_size_request(350, -1)
@@ -82,10 +74,7 @@ class AbstractRow(bases.BaseChildWidget):
         tooltip = self._avatarTooltip()
         if tooltip:
             vbox.set_tooltip_markup(tooltip)
-
-
         vbox.show()
-
         container.pack_start(vbox, expand=False, fill=False, padding=3)
 
     def renderTo(self, parent, start=False):
@@ -99,6 +88,124 @@ class AbstractRow(bases.BaseChildWidget):
         _method(container)
 
 
+class RoundedBox(gtk.EventBox):
+    color = 'white'
+    padding = 2 # Padding from edges of parent
+    rounded = 10 # How round to make the edges
+
+    def __init__(self, *args, **kwargs):
+        super(RoundedBox, self).__init__(*args, **kwargs)
+
+        self.connect('size-allocate', self._on_size_allocate)
+        self.modify_bg(gtk.STATE_NORMAL, 
+                    self.get_colormap().alloc_color(self.color))
+
+    def _on_size_allocate(self, win, allocation):
+        """Shape the window into a rounded rectangle."""
+        w,h = allocation.width, allocation.height
+        bitmap = gtk.gdk.Pixmap(None, w, h, 1)
+
+        # Clear the bitmap
+        fg = gtk.gdk.Color(pixel=0)
+        bg = gtk.gdk.Color(pixel=-1)
+        fg_gc = bitmap.new_gc(foreground=fg, background=bg)
+        bitmap.draw_rectangle(fg_gc, True, 0, 0, w, h)
+
+        # Draw our shape into the pixmap using cairo
+        # Let's try drawing a rectangle with rounded edges.
+        padding = self.padding
+        rounded = self.rounded
+        cr = bitmap.cairo_create()
+        cr.set_source_rgb(0,0,0)
+        # Move to top corner
+        cr.move_to(0+padding+rounded, 0+padding)
+
+        # Top right corner and round the edge
+        cr.line_to(w-padding-rounded, 0+padding)
+        cr.arc(w-padding-rounded, 0+padding+rounded, rounded, math.pi/2, 0)
+        # Bottom right corner and round the edge
+        cr.line_to(w-padding, h-padding-rounded)
+        cr.arc(w-padding-rounded, h-padding-rounded, rounded, 0, math.pi/2)
+        # Bottom left corner and round the edge.
+        cr.line_to(0+padding+rounded, h-padding)
+        cr.arc(0+padding+rounded, h-padding-rounded, rounded, math.pi+math.pi/2, math.pi)
+        # Top left corner and round the edge
+        cr.line_to(0+padding, 0+padding+rounded)
+        cr.arc(0+padding+rounded, 0+padding+rounded, rounded, math.pi/2, 0)
+        # Fill in the shape.
+        cr.fill()
+        # Set the window shape
+        win.shape_combine_mask(bitmap, 0, 0)
+
+class Status(object):
+    text = None
+    timestamp = None
+
+    def __init__(self, text, when, **kwargs):
+        self.text = text
+        self.timestamp = when
+
+    def _markup_generator(self, pieces):
+        for piece in pieces:
+            if not piece:
+                continue
+            if piece.startswith('@') and len(piece) >= 2:
+                yield '<b>%s</b>' % piece
+            elif piece.startswith('http://'):
+                yield '<a href="%s"><b>%s</b></a>' % (piece, piece)
+            elif piece.startswith('#') and len(piece) >= 3:
+                # If it's a two-character or larger hashtag, mark it up
+                yield '<a href="tweepy://search/%s">%s</a>' % (''.join(piece[1:]), piece)
+            elif len(piece) > 2:
+                if piece[0] == '_' and piece[-1] == '_':
+                    # pieces that are _underlined_ should be emphasized
+                    yield '<i>%s</i>' % piece[1:-1]
+                elif piece[0] == '*' and piece[-1] == '*':
+                    # pieces that are *bolded* should be 
+                    yield '<b>%s</b>' % piece[1:-1]
+                else:
+                    yield piece
+            else:
+                yield piece
+
+    def markup_text(self, text):
+        status = util.escape(text)
+        pieces = status.split(' ')
+        return ' '.join(self._markup_generator(pieces))
+
+    def clickedLink(self, label, uri, data, **kwargs):
+        if not uri.startswith('tweepy://'):
+            return False
+
+        command = uri[9:]
+        command, arg = command.split('/')
+        if command == 'search':
+            util.get_global('app')._spawnSearch('#%s' % arg)
+        return True
+
+    def widget(self):
+        roundedbox = RoundedBox()
+        roundedbox.show()
+
+        labelbox = gtk.EventBox()
+        labelbox.set_border_width(5)
+        labelbox.modify_bg(gtk.STATE_NORMAL, 
+                labelbox.get_colormap().alloc_color(roundedbox.color))
+
+        what = gtk.Label()
+        what.set_markup('%s    <i><span size="x-small" weight="light">%s</span></i>' %
+                (self.markup_text(self.text), self.timestamp))
+        what.connect('activate-link', self.clickedLink, None)
+        what.set_size_request(280, -1)
+        what.set_line_wrap(True)
+        what.set_selectable(True)
+        what.set_alignment(0.0, 0.0)
+        labelbox.add(what)
+        roundedbox.add(labelbox)
+        labelbox.show()
+        what.show()
+        return roundedbox
+
 
 class BasicRow(AbstractRow):
     @classmethod
@@ -106,17 +213,8 @@ class BasicRow(AbstractRow):
         return True
 
     def _renderStatus(self, container):
-        what = gtk.Label()
-        self.what = self._markupStatus(self.what)
-        what.set_markup('%s    <i><span size="x-small" weight="light">%s</span></i>' % (
-                self.what, self.when))
-        what.connect('activate-link', self.clickedLink, None)
-        what.set_size_request(280, -1)
-        what.set_line_wrap(True)
-        what.set_selectable(True)
-        what.set_alignment(0.0, 0.0)
-        what.show()
-        container.pack_start(what, expand=True, fill=True)
+        self.status = Status(self.what, self.when)
+        container.pack_start(self.status.widget(), expand=True, fill=True)
 
     def _render(self, container):
         self._renderAvatar(container)
@@ -132,35 +230,6 @@ class BasicRow(AbstractRow):
         'description' : util.escape(self.user['description']),
         'time_zone' : util.escape(self.user['time_zone']),
         'following' : self.user['following'] and _('yes') or _('no')}
-
-
-    def _markupStatus(self, status):
-        status = util.escape(status)
-        pieces = status.split(' ')
-        def markup():
-            for piece in pieces:
-                if not piece:
-                    continue
-                if piece.startswith('@') and len(piece) >= 2:
-                    yield '<b>%s</b>' % piece
-                elif piece.startswith('http://'):
-                    yield '<a href="%s"><b>%s</b></a>' % (piece, piece)
-                elif piece.startswith('#') and len(piece) >= 3:
-                    # If it's a two-character or larger hashtag, mark it up
-                    yield '<a href="tweepy://search/%s">%s</a>' % (''.join(piece[1:]), piece)
-                elif len(piece) > 2:
-                    if piece[0] == '_' and piece[-1] == '_':
-                        # pieces that are _underlined_ should be emphasized
-                        yield '<i>%s</i>' % piece[1:-1]
-                    elif piece[0] == '*' and piece[-1] == '*':
-                        # pieces that are *bolded* should be 
-                        yield '<b>%s</b>' % piece[1:-1]
-                    else:
-                        yield piece
-                else:
-                    yield piece
-
-        return ' '.join(markup())
 
 views.insert(0, BasicRow)
 
