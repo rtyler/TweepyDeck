6596ac850072165510425869e33b68363b7feecf
R. Tyler Ballance <tyler@monkeypox.org>
2010-01-30 01:35:55 -0800

    Slightly update the image caching code to denote that the file came from TweepyDeck

--- TweepyDeck/util.py
+++ TweepyDeck/util.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+from __future__ import with_statement
 
 import os
 import sys
 import time
 import urllib2
-
 
 def __global_prefix(key):
     return '_TweepyDeck_%s' % key
@@ -18,18 +18,24 @@ def readable_time():
     return time.strftime('%H:%M:%S', time.localtime())
 
 def cachedImagePath(who):
-    return '/tmp/%s.png' % who
+    return '/tmp/tweepydeck_%s.png' % who
 
 def saveImageToFile(who, img_url):
     img = cachedImagePath(who)
     if os.path.exists(img):
         return img
 
-    web_fd = urllib2.urlopen(img_url)
-    data = web_fd.read()
-    web_fd.close()
+    try:
+        web_fd = urllib2.urlopen(img_url)
+        data = web_fd.read()
+        web_fd.close()
+    except urllib2.HTTPError:
+        # Damnits.
+        return
 
-    fd = open(img, 'w')
-    fd.write(data)
-    fd.close()
+    if not data:
+        return
+
+    with open(img, 'w') as fd:
+        fd.write(data)
     return img
